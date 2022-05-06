78381be5a0f36141b7d82864f6455e9847e472ec
Pooja Babu <p.babu@fz.juelich.de>
2022-05-06 10:10:02 +0200

    Remove nestrc.sli from build.sh

--- build.sh
+++ build.sh
@@ -63,11 +63,6 @@ echo "--> Detected PYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR"
 
 # Explicitly allow MPI oversubscription. This is required by Open MPI versions > 3.0.
 # Not having this in place leads to a "not enough slots available" error.
-cp extras/nestrc.sli ~/.nestrc
-# if [[ "$OSTYPE" = "darwin"* ]] ; then
-#   sed -i -e 's/mpirun -np/mpirun --oversubscribe -np/g' ~/.nestrc
-# fi
-sed -i -e 's/mpirun -np/mpirun --oversubscribe -np/g' ~/.nestrc
 NEST_RESULT=result
 if [ "$(uname -s)" = 'Linux' ]; then
     NEST_RESULT=$(readlink -f $NEST_RESULT)
